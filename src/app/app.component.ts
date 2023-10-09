c38ec07d9235bd8cae85f8d8b6c286f3cc918824
Suresh Siddagari <Suresh.Siddagari@csgi.com>
2023-10-09 21:47:39 +0200

    sl api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'SLTravelPlan';
+	constructor(private http: HttpClient) {}
+	ngOnInit(): void {
+		const testUrl = 'https://api.sl.se/api2/realtimedeparturesV4.json?key=e2a856b551654e4093432b07ab061962&siteid=9104&timewindow=60';
+		this.http.get(testUrl).subscribe(
+			(data) => {
+				console.log(data);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}
 }
